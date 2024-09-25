ba254b093470d3f7f61dc429de3a1c152008afd0
Young-Yeol Cho <60500365+choyoungyeol@users.noreply.github.com>
2024-09-25 10:33:46 +0900

    Create 6.ino

---
+++ 6.ino
@@ -0,0 +1,23 @@
+#include "DHT.h"
+#define DHTPIN 2              
+#define DHTTYPE DHT22      
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);             
+  dht.begin();
+  delay(300);
+}
+
+void loop() {
+  float humi = dht.readHumidity();
+  float temp = dht.readTemperature();
+  
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print(" oC, Humidity: ");
+  Serial.print(humi);
+  Serial.println(" %");
+  delay(5000);
+}
