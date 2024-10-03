cd3f49c791786a972bd52bbc39b54352c9bd3e4c
Young-Yeol Cho <60500365+choyoungyeol@users.noreply.github.com>
2024-10-03 11:11:09 +0900

    Create Light_BH1750.ino

---
+++ Light_BH1750.ino
@@ -0,0 +1,18 @@
+#include <BH1750.h>
+#include <Wire.h>
+
+BH1750 lightMeter;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  lightMeter.begin();
+}
+
+void loop() {
+  float lux = lightMeter.readLightLevel();
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  delay(1000);
+}
